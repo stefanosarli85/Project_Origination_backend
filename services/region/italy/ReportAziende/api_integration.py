680eb19cfe7c4776d59ee5bc2edf739f4ca3156b
rahulkrguptave <rkgupta@virtualemployee.com>
2026-05-20 18:39:36 +0530

    italy apis

--- services/region/italy/ReportAziende/api_integration.py
+++ services/region/italy/ReportAziende/api_integration.py
@@ -1,11 +1,17 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv("REPOT_AZIENDE_TOKEN")
 
 
-def     get_company_details_form_reportaziende(cid: str):
+def get_company_details_form_reportaziende(cid: str):
     url = f"https://api.reportaziende.it/dettaglioazienda/visualizza/{cid}/05,10,20,30,40,70,85,ANA"
 
     headers = {
-        "Authorization": "Bearer 49da9d58db14ad65747ac3b90d80f8a7f8ada6b59645a86f8fd1a26c2a16070c"
+        "Authorization": f"Bearer {token}"
     }
 
     try:
